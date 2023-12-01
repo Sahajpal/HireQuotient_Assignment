b11bff1a8b10c49863ee28e44cc104751591036c
Sahaj Pal Singh Mahla <54036040+Sahajpal@users.noreply.github.com>
2023-12-01 21:50:28 +0530

    feat: Implemented Api request using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from 'axios';
+
+export type Data = {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+};
 
 function App() {
+  const [data, setData] = useState<Data[] | null>(null);
+
+  useEffect(() => {
+    const url =
+      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+    axios.get(url).then((response) => {
+      setData(response.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data
+        ? data.map((single) => {
+            return <p>{single.id + " " + single.name + " " + single.email + " " + single.role}</p>;
+          })
+        : null}
     </div>
   );
 }
